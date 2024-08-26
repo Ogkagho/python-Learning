248e335965a6dffc7f5bc2f64a8d7f34fbf714b0
Ogkagho <147190469+Ogkagho@users.noreply.github.com>
2024-08-26 21:22:52 +0200

    Delete python_programming_Robert_sedgewick directory

--- python_programming_Robert_sedgewick/Chapter_1/Cmdline.py
+++
@@ -1,6 +0,0 @@
-import sys
-import stdio
-
-stdio.write('Hi, ')
-stdio.write(sys.argv[1])
-stdio.write('. How are you?')
--- python_programming_Robert_sedgewick/Chapter_1/userargumentthree.py
+++
@@ -1,9 +0,0 @@
-import sys
-import stdio
-
-stdio.write('Hi ')
-stdio.write(sys.argv[1])
-stdio.write(', ')
-stdio.write(sys.argv[2])
-stdio.write(', and ')
-stdio.write(sys.argv[3])
