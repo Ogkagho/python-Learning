a17866523e8ac31d2655c2910af3049506930bd3
Ogkagho <147190469+Ogkagho@users.noreply.github.com>
2024-09-14 13:30:21 +0200

    Update Computer_Aided_Instruction(1).py added input validation

--- Tiny_Projects/Computer_Aided_Instruction(1).py
+++ Tiny_Projects/Computer_Aided_Instruction(1).py
@@ -7,14 +7,23 @@ Created on Thu Aug 22 14:23:48 2024
 import random
 
 def intgenerator(c):
-    num1 = random.randrange(1,10**c)
-    num2 = random.randrange(1,10**c)
+    num1 = random.randrange(10**(c-1),10**c)
+    num2 = random.randrange(10**(c-1),10**c)
     
     return num1, num2
 
 valid = input('Start game (1): ')
+
 while valid != '0':
+    while valid != "1":
+        print("Please enter 1 to start game: ")
+        valid = input('Start game (1): ')
+        
     difficulty = int(input('Select difficulty level, 1 or 2: '))
+    while difficulty != 1 and difficulty != 2:
+        print("Invalid input.\n Enter a valid input")
+        difficulty = int(input('Select difficulty level, 1 or 2: '))
+        
     n1, n2 = intgenerator(difficulty)
     ans = int(input(f'How much is {n1} times {n2} ? '))
     
@@ -27,9 +36,21 @@ while valid != '0':
         else:
             print('Keep up the good Work')
     else:
-        while ans != n1*n2:
-            print('No. Please try again.')
-            ans = int(input(f'How much is {n1} times {n2}? '))
+        entry = 1
+        while ans != n1*n2 or entry != 0:
+            choice = random.randrange(1,4)
+            if choice == 1:
+                print('No. Please try again.')
+            elif choice == 2:
+                print("Wrong, you can do it try again")
+            else:
+                print("Incorrect")
+            entry = input("Enter (0) to quit, any other integer to continue: ")
+            if entry != "0":
+                ans = int(input(f'How much is {n1} times {n2}? '))
+            else:
+                print("Sorry to see you go")
+            
         choice = random.randrange(1,4)
         if choice == 1:
             print('Very good')
